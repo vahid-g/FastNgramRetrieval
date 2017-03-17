aad717489be50a866ec6ecd6c0fdf194f129a8d0
vahid-g <vahid87@gmail.com>
2017-03-17 15:43:45 -0700

    complete single threaded version

--- src/main/Engine.java
+++ src/main/Engine.java
@@ -3,7 +3,6 @@ package main;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Engine {
 
@@ -12,12 +11,28 @@ public class Engine {
 		try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
 			String line = br.readLine();
 			while(!line.equals("S")){
-				root.addKid(line);
+				root.addKid(line);		
+				line = br.readLine();
 			}
 			line = br.readLine();
+			while(!(line == null)){
+				if (line.charAt(0) == 'Q'){
+					System.out.println(root.search(line.substring(2)));
+				} else if (line.charAt(0) == 'A'){
+					root.addKid(line.substring(2));
+				} else if (line.charAt(0) == 'D') {
+					root.removeKid(line.substring(2));
+				} else if (line.charAt(0) == 'F') {
+					break;
+				} else {
+					System.err.println("wrong input format");
+					break;
+				}
+				line = br.readLine();
+			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		System.out.println(root);
+		// System.out.println(root);
 	}
 }
--- src/main/TrieNode.java
+++ src/main/TrieNode.java
@@ -1,7 +1,7 @@
 package main;
 
-import java.util.Arrays;
 import java.util.HashMap;
+import java.util.StringTokenizer;
 
 public class TrieNode {
 
@@ -16,7 +16,7 @@ public class TrieNode {
 		this.isValid = isValid;
 	}
 
-	public void addKid(String rawNgram) {
+	public void addKidRec(String rawNgram) {
 		String ngram = rawNgram.trim();
 		int spaceIndex = ngram.indexOf(" ");
 		if (spaceIndex == -1) {
@@ -33,13 +33,80 @@ public class TrieNode {
 			if (node == null) {
 				node = new TrieNode(first, false);
 			}
-			node.addKid(rest);
+			kids.put(first, node);
+			node.addKidRec(rest);
 		}
+	}
+	
+	public void addKid(String rawNgram){
+		StringTokenizer st = new StringTokenizer(rawNgram);
+		TrieNode node = this;
+		while (st.hasMoreTokens()){
+			String token = st.nextToken();
+			if (node.kids.containsKey(token)){
+				node = node.kids.get(token);
+			} else {
+				TrieNode newNode = new TrieNode(token, false);
+				node.kids.put(token, newNode);
+				node = newNode;
+			}
+		}
+		node.isValid = true;
+	}
+	
+	public void removeKid(String rawNgram){
+		StringTokenizer st = new StringTokenizer(rawNgram);
+		TrieNode node = this;
+		while(st.hasMoreTokens()){
+			String token = st.nextToken();
+			if (node.kids.containsKey(token)){
+				node = node.kids.get(token); 
+			} else {
+				break;
+			}
+		}
+		if (!st.hasMoreTokens())
+			node.isValid = false;
+	}
+	
+	public String search(String doc) {
+		StringBuilder resultBuilder = new StringBuilder();
+		int ndx = doc.indexOf(' ');
+		while (ndx != -1){
+			String r = singleSearch(doc);
+			resultBuilder.append(r);
+			doc = doc.substring(ndx + 1);
+			ndx = doc.indexOf(' ');
+		}
+		resultBuilder.append(singleSearch(doc));
+		try{
+			resultBuilder.deleteCharAt(resultBuilder.length() - 1);
+		} catch (StringIndexOutOfBoundsException e){
+			
+		}
+		
+		return resultBuilder.length() > 0 ? resultBuilder.toString() : "-1";
+	}
+	
+	public String singleSearch(String doc){
+		TrieNode node = this;
+		StringTokenizer st = new StringTokenizer(doc);
+		StringBuilder sb = new StringBuilder();
+		StringBuilder result = new StringBuilder();
+		while(st.hasMoreTokens()){
+			String token = st.nextToken();
+			node = node.kids.get(token);
+			if (node == null) break;
+			sb.append(token + " ");
+			if (node.isValid) result.append(sb.toString().trim() + "|");
+		}
+		return result.toString();
+		
 	}
 	
 	@Override
 	public String toString() {
-		String rep = "term: " + term + "\n" + kids.toString();
+		String rep = "term: " + term + " " + isValid + "\n\t" + kids.toString();
 		return rep;
 	}
 }
